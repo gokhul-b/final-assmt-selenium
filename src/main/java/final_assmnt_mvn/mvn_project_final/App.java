60d2a3733b1f164a259bd802e55e0f3a3f994c36
gokhul-b <gokhulb2001@gmail.com>
2024-04-12 18:50:48 +0530

    first

---
+++ src/main/java/final_assmnt_mvn/mvn_project_final/App.java
@@ -0,0 +1,13 @@
+package final_assmnt_mvn.mvn_project_final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/final_assmnt_mvn/mvn_project_final/SeleniumTestSample.java
@@ -0,0 +1,43 @@
+package final_assmnt_mvn.mvn_project_final;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class SeleniumTestSample {
+	private WebDriver driver;
+    @BeforeMethod
+    public void setUp() {
+    	System.setProperty("webdriver.chrome.driver","D:\\Webdriver\\chromedriver_win64\\chromedriver.exe");
+    	driver = new ChromeDriver();
+    }
+    @Test
+    public void googleSearchTest() {
+        driver.get("https://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("Selenium");
+        searchBox.submit();
+        // Wait for search results to load
+        try {
+            Thread.sleep(2000); // Wait for 2 seconds (for demonstration purposes)
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Verify the page title contains the search query
+        String pageTitle = driver.getTitle();
+        Assert.assertTrue(pageTitle.contains("Selenium"), "Search results page title does not contain 'Selenium'");
+    }
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
